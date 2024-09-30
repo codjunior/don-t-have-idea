9635a0731df4dcab9a7f53f7f87ffdfc37de8180
codjunior <157795824+codjunior@users.noreply.github.com>
2024-09-30 19:43:49 +0330

    Add files via upload

---
+++ guess_number_game.py
@@ -0,0 +1,14 @@
+import datetime as dt
+
+date = dt.Now()
+print(date)
+
+# try:
+#     with open('rates.md', 'a+') as f:
+#         f.write('hello\n')
+#         print('done')
+# except ValueError:
+#     print('error')
+
+
+
